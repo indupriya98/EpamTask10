2aa4abf2a6a438bf1ddcc5441fb17ff53f6972e3
Indupriya Chegireddy <57555303+indupriya98@users.noreply.github.com>
2020-07-21 20:19:11 +0530

    Final Commit

--- src/com/company/Filter_strings.java
+++ src/com/company/Filter_strings.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 
 
 public class Filter_strings {
-    public static List<String> count_strings(List<String> list_of_strings) {
+    public static List<String> checking_strings(List<String> list_of_strings) {
         return list_of_strings.stream().filter(x -> (x.startsWith("a") && x.length()==3 )).collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        List<String> strList = Arrays.asList("abc", "abcd", "bbcd", "aabb", "defg", "jk","i","avb","aaaa");
-        List<String> res= count_strings(strList);
+        List<String> input_list = Arrays.asList("abc", "abcd", "bbcd", "aabb", "defg", "jk","i","avb","aaaa");
+        List<String> res= checking_strings(input_list);
         System.out.println(res);
     }
 
